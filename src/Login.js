5331d02b154ef50aa7bf35e4e34838a155192bff
shefeek1 <shefeek807@gmail.com>
2022-07-25 12:43:53 +0530

    First Commit

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <input type ="text" placeholder='username'/>
+      <input type ="text" placeholder='Date of Birth' />
+      <button>Submit</button>
+      
+          </div>
+  )
+}
+
+export default Login
---
+++ src/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile({authorized}) {
+  return (
+    <div>
+<h2>  Marks  </h2>
+    </div>
+  )
+}
+
+export default Profile
